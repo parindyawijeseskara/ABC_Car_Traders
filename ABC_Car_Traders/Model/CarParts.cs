f02e7ec60f6fbac182c8903d423f08f00a9f836b
parindyawijeseskara <parindyawijesekara@gmail.com>
2024-08-08 10:27:51 +0530

    Add model Car Parts.

---
+++ ABC_Car_Traders/Model/CarParts.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ABC_Car_Traders.Model
+{
+    public class CarParts
+    {
+        public int carPartId { get; set; }
+        public string carPartName { get; set;}
+        public byte[] image { get; set;}
+        public string model {  get; set;}
+        public decimal price { get; set;}
+        public int quantity { get; set;}
+        public string description { get; set;}
+        public string manufacturer { get; set;}
+        public string warrantyPeriod { get; set;}
+        public string status { get; set;}
+        public DateTime createdAt { get; set; }
+        public DateTime updatedAt { get; set; }
+        public DateTime? deletedAt { get; set; }
+    }
+}
