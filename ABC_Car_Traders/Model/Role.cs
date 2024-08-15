bfb5277bedbbf5a574df8b50275b0f76438aa36a
parindyawijeseskara <parindyawijesekara@gmail.com>
2024-08-15 18:17:51 +0530

    Add model Role.

---
+++ ABC_Car_Traders/Model/Role.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ABC_Car_Traders.Model
+{
+    public class Role
+    {
+        public int roleId { get; set; }
+        public string role { get; set; }
+    }
+}
